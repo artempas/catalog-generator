322f17a602958c241472ac1d249dc60b11d40ea9
Artem P <pasechnik@1060.ru>
2024-12-24 16:13:47 +0300

    `Added support for filtering and sorting in HTML catalog, improved modal window for poster images, and calculated total duration of all movies.`

--- generate-catalog.py
+++ generate-catalog.py
@@ -12,22 +12,23 @@ DUMP_JSON = {}
 
 def get_video_files():
     """Получает список видеофайлов в директории и обрезает расширения."""
-    video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.stub']
+    video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.stub', ".m4v"]
     video_files = []
     for file in os.listdir(BASE_PATH):
         if os.path.splitext(file)[1].lower() in video_extensions:
             video_files.append(file)
     return video_files
 
-def search_movie(api_url, api_token, file_name, query=None):
+def search_movie(api_url, api_token, file_name: str, query=None):
     """Ищет информацию о фильме через API Kinopoisk Dev."""
     global DUMP_JSON
     headers = {
         'X-API-KEY': api_token
     }
     if file_name in DUMP_JSON:
+        print('Cached')
         return DUMP_JSON[file_name]
-    movie_name = (os.path.splitext(file_name)[0] if not query else query).replace('.',' ')
+    movie_name = (os.path.splitext(file_name)[0].replace('.fixed','') if not query else query).replace('.',' ')
     extension = os.path.splitext(file_name)[1]
     params = {
         'query': movie_name,
@@ -38,7 +39,7 @@ def search_movie(api_url, api_token, file_name, query=None):
         data = response.json()
         if data.get('docs'):
             index = 0
-            if re.match(r'.* \([0-9]{4}\)\.[a-z]{3}', file_name) is None:
+            if re.match(r'.* \([0-9]{4}\)\.[a-z]{3}', file_name) is None and not file_name.split('.')[-2] == 'fixed':
                 index = input('\n'.join([
                     f"{i + 1}. {data['docs'][i].get('name')} ({data['docs'][i].get('year')}) / {data['docs'][i].get('alternativeName')}"
                     for i in range(len(data['docs']))
@@ -49,9 +50,12 @@ def search_movie(api_url, api_token, file_name, query=None):
                         return None
                 else:
                     return search_movie(api_url, api_token, file_name, index)
-            new_filename = f"{data['docs'][int(index)]['name']} ({data['docs'][index]['year']}){extension}"
-            new_filename = re.sub(r'[\\/*?:"<>|]', "", new_filename)
-            os.rename(os.path.join(BASE_PATH, file_name), os.path.join(BASE_PATH, new_filename))
+            if file_name.split('.')[-2] == 'fixed':
+                new_filename = file_name
+            else:
+                new_filename = f"{data['docs'][int(index)]['name']} ({data['docs'][index]['year']}){extension}"
+                new_filename = re.sub(r'[\\/*?:"<>|]', "", new_filename)
+                os.rename(os.path.join(BASE_PATH, file_name), os.path.join(BASE_PATH, new_filename))
             DUMP_JSON[new_filename]=data['docs'][index]
             return data['docs'][index]
         else:
@@ -74,6 +78,9 @@ def download_poster(poster_url, movie_name):
     # Получаем имя файла из URL
     response = requests.get(poster_url)
     file_path = os.path.join(directory, movie_name + '.' + response.headers['Content-type'].split('/')[1])
+    if os.path.exists(file_path):
+        print('Img cached')
+        return file_path
 
     # Скачиваем изображение
     if response.status_code == 200:
@@ -83,7 +90,7 @@ def download_poster(poster_url, movie_name):
     return None
 
 
-def save_catalog_html(catalog, output_file):
+def save_catalog_html(catalog, output_file, total_duration):
     """Сохраняет каталог фильмов в формате HTML с фильтрацией и сортировкой."""
     # Собираем уникальные годы, жанры и страны для селекторов
     years = sorted(set(movie['Год'] for movie in catalog if movie['Год'] != 'Неизвестно'))
@@ -103,6 +110,42 @@ def save_catalog_html(catalog, output_file):
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Каталог фильмов</title>
         <style>
+            .modal {
+                display: none; 
+                position: fixed; 
+                z-index: 1000; 
+                left: 0;
+                top: 0;
+                width: 100%; 
+                height: 100%; 
+                overflow: auto; 
+                background-color: rgb(0,0,0); 
+                background-color: rgba(0,0,0,0.9); 
+            }
+
+            .modal-content {
+                margin: auto;
+                display: block;
+                width: auto;
+                height: 100%;
+            }
+
+            .close {
+                position: absolute;
+                top: 15px;
+                right: 35px;
+                color: #fff;
+                font-size: 40px;
+                font-weight: bold;
+                transition: 0.3s;
+            }
+
+            .close:hover,
+            .close:focus {
+                color: #bbb;
+                text-decoration: none;
+                cursor: pointer;
+            }
             body {
                 font-family: sans-serif;
             }
@@ -141,20 +184,21 @@ def save_catalog_html(catalog, output_file):
                 border: 1px solid #ddd;
             }
             .rating-high {
-                color: #28b328;
+                color: #180101;
                 font-weight: bold;
+                background: #0099298f;
             }
             .rating-good {
                 font-weight: bold;
-                color: #85c151;
+                background: #60e90c82;
             }
             .rating-medium {
                 font-weight: bold;
-                color: #ffd600;
+                color: #585858;
             }
             .rating-low {
                 font-weight: bold;
-                color: #FF0000;
+                color: #000000;
             }
             .asc::after {
                 content: "▲";
@@ -179,9 +223,15 @@ def save_catalog_html(catalog, output_file):
                 }
 
                 rows.sort((a, b) => {
-                    const aText = a.cells[columnIndex].innerText;
-                    const bText = b.cells[columnIndex].innerText;
-                    return currentSortOrder === 'asc' ? aText.localeCompare(bText) : bText.localeCompare(aText);
+                    let aint = parseInt(a.cells[columnIndex].innerText);
+                    let bint = parseInt(b.cells[columnIndex].innerText);
+                    if (isNaN(aint) || isNaN(bint)) {
+                        const aText = a.cells[columnIndex].innerText;
+                        const bText = b.cells[columnIndex].innerText;
+                        return currentSortOrder === 'asc' ? aText.localeCompare(bText) : bText.localeCompare(aText);
+                    } else {
+                        return currentSortOrder === 'asc' ? aint - bint : bint - aint;
+                    }
                 });
                 
                 table.tBodies[0].append(...rows);
@@ -200,7 +250,7 @@ def save_catalog_html(catalog, output_file):
 
             function filterTable() {
                 const genreSelect = document.getElementById("genreFilter").value;
-                const countrySelect = Array.from(document.getElementById("countryFilter").selectedOptions).map(opt => opt.value);
+                const countrySelect = document.getElementById("countryFilter").value;
                 const yearAfter = document.getElementById("yearAfter").value;
                 const yearBefore = document.getElementById("yearBefore").value;
                 const nameInput = document.getElementById("nameFilter").value.toLowerCase();
@@ -208,12 +258,12 @@ def save_catalog_html(catalog, output_file):
 
                 rows.forEach(row => {
                     const genres = row.cells[3].innerText.split(',').map(g => g.trim());
-                    const countries = row.cells[7].innerText.split(',').map(c => c.trim());
+                    const countries = row.cells[6].innerText.split(',').map(c => c.trim());
                     const year = row.cells[1].innerText;
                     const name = row.cells[0].innerText.toLowerCase();
 
-                    const genreMatch = !genreSelect || genres.includes(genreSelect);
-                    const countryMatch = !countrySelect.length || countrySelect.some(country => countries.includes(country));
+                    const genreMatch = !genreSelect || genres.includes(genreSelect.trim());
+                    const countryMatch = !countrySelect || countries.includes(countrySelect.trim());
                     const yearMatch = (!yearAfter || year >= yearAfter) && (!yearBefore || year <= yearBefore);
                     const nameMatch = !nameInput || name.includes(nameInput);
 
@@ -223,19 +273,18 @@ def save_catalog_html(catalog, output_file):
         </script>
     </head>
     <body>
-        <h1>Каталог фильмов</h1>
+        <h1>Каталог фильмов</h1>""" + f"<h3>Всего фильмов: {len(catalog)}, Суммарная продолжительность: {total_duration // 60}ч  {total_duration % 60}мин</h3>"+ """
         <div class="filters">
             <div class="filter-group">
                 <label for="nameFilter">Поиск по названию:</label>
                 <input type="text" id="nameFilter" onkeyup="filterTable()">
             </div>
             <div class="filter-group">
-                <label for="yearAfter">Год (после):</label>
-                <input type="number" id="yearAfter" onchange="filterTable()">
-            </div>
-            <div class="filter-group">
-                <label for="yearBefore">Год (до):</label>
-                <input type="number" id="yearBefore" onchange="filterTable()">
+                <label for="yearAfter">Год:</label>
+                <input style="width: 75px" type="number" id="yearAfter" onchange="filterTable()">
+
+                <label for="yearBefore">-</label>
+                <input style="width: 75px" type="number" id="yearBefore" onchange="filterTable()">
             </div>
             <div class="filter-group">
                 <label for="genreFilter">Жанр:</label>
@@ -252,7 +301,7 @@ def save_catalog_html(catalog, output_file):
             </div>
             <div class="filter-group">
                 <label for="countryFilter">Страна:</label>
-                <select id="countryFilter" multiple onchange="filterTable()">
+                <select id="countryFilter" onchange="filterTable()">
                     <option value="">Все страны</option>
     """
 
@@ -269,10 +318,10 @@ def save_catalog_html(catalog, output_file):
                 <tr>
                     <th onclick="sortTable(0)">Название</th>
                     <th onclick="sortTable(1)">Год</th>
-                    <th onclick="sortTable(2)">Описание</th>
+                    <th>Описание</th>
                     <th>Жанр</th>
                     <th onclick="sortTable(4)">Оценка</th>
-                    <th onclick="sortTable(5)">Продолжительность</th>
+                    <th onclick="sortTable(5)">Продолжительность, мин</th>
                     <th>Страна</th>
                     <th>Постер</th>
                 </tr>
@@ -286,22 +335,41 @@ def save_catalog_html(catalog, output_file):
             rating_class = 'rating-high' if rating > 8 else 'rating-good' if rating > 7 else 'rating-medium' if rating > 5 else 'rating-low'
         except (ValueError, TypeError):
             rating_class = ''
+        poster_arg = movie['Постер'].replace('\\','\\\\')
         html_content += f"""
                 <tr>
-                    <td>{movie['Название']}</td>
+                    <td><a href="{movie['Ссылка']}">{movie['Название']}</td>
                     <td>{movie['Год']}</td>
                     <td>{movie['Описание']}</td>
                     <td>{movie['Жанр']}</td>
                     <td class="{rating_class}">{movie['Оценка']}</td>
                     <td>{movie['Продолжительность']}</td>
                     <td>{movie['Страна']}</td>
-                    <td><img src="{movie['Постер']}" alt="Постер фильма"></td>
+                    <td><img src="{movie['Постер']}" alt="Постер фильма" onclick="openModal('{poster_arg}')" style="cursor: pointer;"></td>
                 </tr>
         """
     
     html_content += """
             </tbody>
         </table>
+        <div id="myModal" class="modal">
+            <span class="close" onclick="closeModal()">&times;</span>
+            <img class="modal-content" id="img01">
+        </div>
+
+        <script>
+            function openModal(imgSrc) {
+                const modal = document.getElementById("myModal");
+                const modalImg = document.getElementById("img01");
+                modal.style.display = "block";
+                modalImg.src = imgSrc;
+            }
+
+            function closeModal() {
+                const modal = document.getElementById("myModal");
+                modal.style.display = "none";
+            }
+        </script>
     </body>
     </html>
     """
@@ -325,6 +393,7 @@ def main():
     print(f"Найдено {len(video_files)} видеофайлов.")
 
     catalog = []
+    total_duration = 0
     for movie_name in video_files:
         print(f"Ищем информацию о фильме: {movie_name}")
         movie_info = search_movie(api_url, api_token, movie_name)
@@ -332,8 +401,10 @@ def main():
             poster_url = movie_info.get('poster', {}).get('url', '')
             local_poster = download_poster(poster_url, movie_name) if poster_url else None
             duration = movie_info.get('movieLength', 0)
-            duration = f'{duration // 60}ч {duration % 60}м' if duration > 60 else f'{duration}м' if duration else 'Нет данных'
+            total_duration += duration
+            duration = duration if duration else 'Нет данных'
             countries = ', '.join(i['name'] for i in movie_info.get('countries', []))
+            url = f'https://www.kinopoisk.ru/film/{movie_info.get("id")}/'
             catalog.append({
                 'Название': movie_info.get('name', 'Неизвестно'),
                 'Год': movie_info.get('year', 'Неизвестно'),
@@ -342,12 +413,13 @@ def main():
                 'Оценка': round(float(movie_info.get('rating', {}).get('kp')), 2) if movie_info.get('rating', {}).get('kp') else 'Нет данных',
                 'Продолжительность': duration,
                 'Страна': countries,
-                'Постер': local_poster if local_poster else ''
+                'Постер': local_poster if local_poster else '',
+                'Ссылка': url
             })
         else:
             print(f"Информация о фильме {movie_name} не найдена.")
 
-    save_catalog_html(catalog, os.path.join(BASE_PATH, output_file))
+    save_catalog_html(catalog, os.path.join(BASE_PATH, output_file), total_duration)
     with open('dump.json', 'w') as dump:
         json.dump(DUMP_JSON, dump)
     
